d7c8363ba2d1bae6daf66a690c47b11b8a1f6f82
miki <miki@vaadin.com>
2014-10-01 22:25:23 +0300

    #2 button clicks as operations

--- touchee/src/org/vaadin/miki/touchee/views/AbstractView.java
+++ touchee/src/org/vaadin/miki/touchee/views/AbstractView.java
@@ -5,8 +5,8 @@ import com.vaadin.addon.touchkit.ui.Toolbar;
 import com.vaadin.ui.Button;
 
 /**
- * Base class for all views. Abstracts the logic of buttons and button clicks in the bottom toolbar.
- * 
+ * Base class for all views. Abstracts the logic of buttons and button clicks in the toolbar.
+ *
  * @author miki
  *
  */
@@ -15,6 +15,21 @@ public class AbstractView extends NavigationView {
   private static final long serialVersionUID = 20140930L;
 
   private final Toolbar toolbar = new Toolbar();
+
+  /**
+   * Interface for operations to be broadcasted by the view.
+   *
+   * @author miki
+   *
+   */
+  public static interface Operation {
+
+    /**
+     * Operation was triggered.
+     *
+     */
+    public void perform();
+  }
 
   /**
    * Constructs the view and initialises the toolbar.
@@ -27,7 +42,7 @@ public class AbstractView extends NavigationView {
 
   /**
    * Adds buttons to the toolbar.
-   * 
+   *
    * @param buttons
    *          Buttons to add to the toolbar.
    */
@@ -37,13 +52,36 @@ public class AbstractView extends NavigationView {
 
   /**
    * Removes buttons from the toolbar.
-   * 
+   *
    * @param buttons
    *          Buttons to remove from the toolbar.
    */
   public void removeButton(Button... buttons) {
     for(Button b: buttons)
       this.toolbar.removeComponent(b);
+  }
+
+  /**
+   * Adds a named operation as a button in the toolbar.
+   *
+   * @param name
+   *          Name.
+   * @param operation
+   *          Operation. Usually this is provided by the view itself.
+   * @return Button that was added as a result of the operation.
+   */
+  public Button addOperation(final String name, final Operation operation) {
+    Button button = new Button(name, new Button.ClickListener() {
+
+      private static final long serialVersionUID = 20141001;
+
+      @Override
+      public void buttonClick(Button.ClickEvent event) {
+        operation.perform();
+      }
+    });
+    this.addButton(button);
+    return button;
   }
 
 }
