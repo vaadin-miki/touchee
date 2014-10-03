3e0db338f86d83c1db1d2121fd3ad56d221485b5
Miki <miki@vaadin.com>
2014-10-03 12:21:57 +0300

    #2 events removed from edit view

--- touchee/src/org/vaadin/miki/touchee/views/edit/EditView.java
+++ touchee/src/org/vaadin/miki/touchee/views/edit/EditView.java
@@ -1,7 +1,5 @@
 package org.vaadin.miki.touchee.views.edit;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -14,10 +12,10 @@ import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.event.Action;
 import com.vaadin.ui.Field;
 import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.Notification;
 
 /**
  * A view for editing an existing object. Any action initiated by this view will have the current state of the data (<tt>Map<String, Object></tt>) as a target.
+ * The underlying item can be obtained through {@link #getItemDataSource()}.
  *
  * @author miki
  *
@@ -29,29 +27,6 @@ public class EditView extends ToucheeView implements Item.Editor {
   private FormBuilder formBuilder;
 
   private FieldGroup fieldGroup;
-
-  private final ArrayList<DataChangeListener> beforeChangeListeners = new ArrayList<DataChangeListener>();
-
-  private final ArrayList<DataChangeListener> afterChangeListeners = new ArrayList<DataChangeListener>();
-
-  private final ArrayList<DataChangeListener> discardChangeListener = new ArrayList<DataChangeListener>();
-
-  // translates commit events from the field group into component-independent
-  // data change listeners
-  private final FieldGroup.CommitHandler commitHandler = new FieldGroup.CommitHandler() {
-
-    private static final long serialVersionUID = 20140925;
-
-    @Override
-    public void preCommit(FieldGroup.CommitEvent commitEvent) throws FieldGroup.CommitException {
-      EditView.this.broadcastEvent(commitEvent.getFieldBinder(), EditView.this.beforeChangeListeners, false);
-    }
-
-    @Override
-    public void postCommit(FieldGroup.CommitEvent commitEvent) throws FieldGroup.CommitException {
-      EditView.this.broadcastEvent(commitEvent.getFieldBinder(), EditView.this.afterChangeListeners, true);
-    }
-  };
 
   /**
    * Constructs the view.
@@ -76,12 +51,7 @@ public class EditView extends ToucheeView implements Item.Editor {
    * Repaints the form.
    */
   protected void rebuildForm() {
-    if(this.fieldGroup != null)
-      this.fieldGroup.removeCommitHandler(this.commitHandler);
-
-    // handle commit events
     this.fieldGroup = this.getFormBuilder().buildFieldGroup();
-    this.fieldGroup.addCommitHandler(this.commitHandler);
 
     // update contents
     FormLayout form = new FormLayout(this.fieldGroup.getFields().toArray(new Field<?>[0]));
@@ -104,31 +74,6 @@ public class EditView extends ToucheeView implements Item.Editor {
   }
 
   /**
-   * Broadcasts an event to given listeners.
-   *
-   * @param fieldGroup
-   *          Field group to take data from.
-   * @param listeners
-   *          Listeners to broadcast the event to.
-   * @param committed
-   *          Whether or not the data is committed to the data source.
-   * @throws FieldGroup.CommitException
-   *           when the data is rejected by the listeners.
-   */
-  protected void broadcastEvent(FieldGroup fieldGroup, Collection<DataChangeListener> listeners, boolean committed) throws FieldGroup.CommitException {
-    DataChangeEvent event = new DataChangeEvent(this.getMapOfData(fieldGroup), fieldGroup.getItemDataSource(), committed);
-    for(DataChangeListener listener: listeners)
-      listener.dataChanged(event);
-    if(event.isRejected()) {
-      StringBuilder messageBuilder = new StringBuilder();
-      // build message
-      for(String message: event.getRejectMessages())
-        messageBuilder.append(message).append(" ");
-      throw new FieldGroup.CommitException(messageBuilder.toString());
-    }
-  }
-
-  /**
    * Commits the data currently in the field group.
    */
   public void commit() {
@@ -145,17 +90,7 @@ public class EditView extends ToucheeView implements Item.Editor {
    * Discards the data in the field group and synchronises them with the item.
    */
   public void discard() {
-    if(this.fieldGroup != null) {
-      this.fieldGroup.discard();
-      // field group does not do anything when the form is discarded, but
-      // this view might want to have some listeners (e.g. to close it)
-      try {
-        this.broadcastEvent(this.fieldGroup, this.discardChangeListener, false);
-      }
-      catch(FieldGroup.CommitException e) {
-        Notification.show("Error", e.getMessage(), Notification.Type.WARNING_MESSAGE);
-      }
-    }
+    this.fieldGroup.discard();
   }
 
   /**
@@ -168,37 +103,24 @@ public class EditView extends ToucheeView implements Item.Editor {
     }
   }
 
+  /**
+   * Returns currently used form builder.
+   * 
+   * @return Currently used form builder.
+   */
   public FormBuilder getFormBuilder() {
     return this.formBuilder;
   }
 
+  /**
+   * Sets the new form builder and rebuilds the form.
+   * 
+   * @param formBuilder
+   *          Form builder.
+   */
   public void setFormBuilder(FormBuilder formBuilder) {
     this.formBuilder = formBuilder;
     this.rebuildForm();
-  }
-
-  public void addBeforeChangeListener(DataChangeListener listener) {
-    this.beforeChangeListeners.add(listener);
-  }
-
-  public void addAfterChangeListener(DataChangeListener listener) {
-    this.afterChangeListeners.add(listener);
-  }
-
-  public void removeBeforeChangeListener(DataChangeListener listener) {
-    this.beforeChangeListeners.remove(listener);
-  }
-
-  public void removeAfterChangeListener(DataChangeListener listener) {
-    this.afterChangeListeners.remove(listener);
-  }
-
-  public void addDiscardChangeListener(DataChangeListener listener) {
-    this.discardChangeListener.add(listener);
-  }
-
-  public void removeDiscardChangeListener(DataChangeListener listener) {
-    this.discardChangeListener.remove(listener);
   }
 
   @Override
